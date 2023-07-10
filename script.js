38430b3fa17c107760b0b81a9bc5c93e9ea5bbfb
houdareda <106600533+houdareda@users.noreply.github.com>
2023-07-10 19:50:48 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,10 +6,3 @@ let host = window.location.protocol + "//" + window.location.host + path;
 let redirectValue = document.getElementById("redDir").value;
 document.getElementById("redDir").value = `${host}/thanksPage.html`;
 
-
-let patharr2 = window.location.pathname.split("/");
-patharr2.splice(-1);
-let path2 = patharr.join("/");
-let host2 = window.location.protocol + "//" + window.location.host + path;
-let redirectValue2 = document.getElementById("redDir2").value;
-document.getElementById("redDir2").value = `${host}/thanksPageproduct.html`;
--- script2.js
+++ script2.js
@@ -4,4 +4,4 @@ patharr.splice(-1);
 let path = patharr.join("/");
 let host = window.location.protocol + "//" + window.location.host + path;
 let redirectValue = document.getElementById("redDir2").value;
-document.getElementById("redDir").value = `${host}/thanksPageproduct.html`;
+document.getElementById("redDir2").value = `${host}/thanksPageproduct.html`;
